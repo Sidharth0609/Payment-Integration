925067edb9d2dab0f71281f8046b5c0dcf1b0a6c
sid <sid@DESKTOP-U7GQOFS>
2021-01-30 17:49:33 +0530

    First Commit

---
+++ spring-boot-payment-app/src/main/java/Payment_Integration/springbootpaymentapp/SpringBootPaymentAppApplication.java
@@ -0,0 +1,13 @@
+package Payment_Integration.springbootpaymentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaymentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPaymentAppApplication.class, args);
+	}
+
+}
---
+++ spring-boot-payment-app/src/test/java/Payment_Integration/springbootpaymentapp/SpringBootPaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Payment_Integration.springbootpaymentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPaymentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
